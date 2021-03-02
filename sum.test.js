a1b4cf8461d86c924f60beb440c0b2f94836cb0f
jr1412594 <jr.medina1412@gmail.com>
2021-03-02 12:08:09 -0700

    greeting

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,10 @@
 const sum = require('./sum');
+const greeting = require('./greeting');
 
 test('adds 1 + 2 to equeal 3', () => {
     expect(sum(1, 2)).toBe(3);
+});
+
+test('prints Welcome to 10 Days of JavaScript!"', () => {
+    expect(greeting('Welcome to 10 days of JavaScript!')).toBe('Welcome to 10 days of JavaScript!');
 });
