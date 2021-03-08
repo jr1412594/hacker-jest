98242ae3927865157a8437ea5a82ead56696b8c0
jr1412594 <jr.medina1412@gmail.com>
2021-03-07 19:26:40 -0700

    this is the polygon class

---
+++ polygonClass.js
@@ -0,0 +1,17 @@
+/*
+ * Implement a Polygon class with the following properties:
+ * 1. A constructor that takes an array of integer side lengths.
+ * 2. A 'perimeter' method that returns the sum of the Polygon's side lengths.
+ */
+class Polygon {
+    constructor(sides) {
+        this.sides = sides;
+        // this.width = width;
+    }
+    perimeter(){
+    let reducer = (acc, currentValue) => acc + currentValue;
+    return this.sides.reduce(reducer)
+        }
+}
+
+let p = new Polygon()
