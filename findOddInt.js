5bb73f7e67b358b3571abd25145914e890a991b5
jr1412594 <jr.medina1412@gmail.com>
2021-03-10 21:41:50 -0700

    mfind odd int

---
+++ findOddInt.js
@@ -0,0 +1,14 @@
+function findOdd(A) {
+    let result = 0;
+    let sortA = A.sort((a,b) => a - b);
+    for(let i = 0; i < sortA.length; i++){
+        for(let j = 0; j < sortA.length; j++){
+            if(sortA[i] === sortA[j]){
+            result++
+            }
+        }
+        if(result % 2 !== 0){
+            return sortA[i]
+        }
+    }
+}
