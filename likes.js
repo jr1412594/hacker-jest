5770503ce7aaab422328e4c801ad119769ac1668
jr1412594 <jr.medina1412@gmail.com>
2021-03-12 11:44:32 -0700

    likes function

---
+++ likes.js
@@ -0,0 +1,13 @@
+function likes(array) {
+    if(array.length === 0){
+      return 'no one likes this'
+    } else if(array.length === 1){
+      return `${array[0]} likes this`
+    } else if(array.length === 2){
+      return `${array[0]} and ${array[1]} like this`
+    } else if(array.length === 3){
+      return `${array[0]}, ${array[1]} and ${array[2]} like this`
+    } else {
+      return `${array[0]}, ${array[1]} and ` + (array.length - 2) + ' others like this'
+    }
+  }
