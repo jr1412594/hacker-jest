8eb087c0fbf79c83788b01d5f8613fc106e94a32
jr1412594 <jr.medina1412@gmail.com>
2021-03-18 21:27:57 -0600

    dup encoder

---
+++ duplicateFinder.js
@@ -0,0 +1,14 @@
+function duplicateEncode(word){
+    let newString = ''
+    word = word.toLowerCase() || word
+    word.split('').filter((x, index) => {
+        if(word.indexOf(x) !== index){
+        newString += ')'
+        }else if(word.lastIndexOf(x) !== index){
+        newString += ')'
+        }else{
+        newString += '('
+        }
+    })
+    return newString
+}
