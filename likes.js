84da49bb9ee911f143d906d230929f3fc1237eae
jr1412594 <jr.medina1412@gmail.com>
2021-03-31 19:28:50 -0600

    refactor likes

--- likes.js
+++ likes.js
@@ -11,3 +11,20 @@ function likes(array) {
         return `${array[0]}, ${array[1]} and ` + (array.length - 2) + ' others like this'
     }
 }
+
+
+///refactor
+function likes(names) {
+    switch(names.length){
+        case 0:
+            return 'no one likes this';
+        case 1:
+            return `${names} likes this`;
+        case 2:
+            return `${names[0]} and ${names[1]} like this`;
+        case 3:
+            return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+        default:
+            return `${names[0]}, ${names[1]} and ${names.length-2} others like this`;
+    }
+}
