e4adc31a2b7f1caacaad0ff1eb4b85aa2bda2a8d
jr1412594 <jr.medina1412@gmail.com>
2021-04-12 20:14:41 -0600

    closer to solution

--- longestSubString.js
+++ longestSubString.js
@@ -7,3 +7,27 @@ const lengthOfLongestSubstring = function(s){
   return count
   }
   console.log(lengthOfLongestSubstring(s))
+
+  let s = "pwwkew";
+// let s = "au";
+// let s = "cdd";
+
+const lengthOfLongestSubstring = function(s){
+  let subStr = "";
+  let start = 0;
+  let end = 0;
+  let max = 0
+  
+    while(end < s.length){
+      if(!subStr.includes(s[end])){
+        subStr += s[end];
+        max = Math.max(subStr.length, max)
+      } else {
+        subStr = subStr.slice(start, end);
+        end++
+      }
+    }
+  return subStr
+}
+// console.log(s.slice(2))
+console.log(lengthOfLongestSubstring(s))
