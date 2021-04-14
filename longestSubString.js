148a15bafd4003c5e1b3e20c472f27e255d09194
jr1412594 <jr.medina1412@gmail.com>
2021-04-13 20:53:50 -0600

    final solution

--- longestSubString.js
+++ longestSubString.js
@@ -1,33 +1,52 @@
-const lengthOfLongestSubstring = function(s){
-    let count = {};
-    s = s.split('')
-   for(let i in s){
-     s[i] !== count[i] ? count[i] = s[i] : false
-   }
-  return count
-  }
-  console.log(lengthOfLongestSubstring(s))
+// const lengthOfLongestSubstring = function(s){
+//     let count = {};
+//     s = s.split('')
+//    for(let i in s){
+//      s[i] !== count[i] ? count[i] = s[i] : false
+//    }
+//   return count
+//   }
+//   console.log(lengthOfLongestSubstring(s))
 
   let s = "pwwkew";
 // let s = "au";
 // let s = "cdd";
 
+// const lengthOfLongestSubstring = function(s){
+//   let subStr = "";
+//   let start = 0;
+//   let end = 0;
+//   let max = 0
+  
+//     while(end < s.length){
+//       if(!subStr.includes(s[end])){
+//         subStr += s[end];
+//         max = Math.max(subStr.length, max)
+//       } else {
+//         subStr = subStr.slice(start, end);
+//         end++
+//       }
+//     }
+//   return subStr
+// }
+// // console.log(s.slice(2))
+// console.log(lengthOfLongestSubstring(s))
+
 const lengthOfLongestSubstring = function(s){
-  let subStr = "";
+  let hash = {};
   let start = 0;
   let end = 0;
   let max = 0
   
     while(end < s.length){
-      if(!subStr.includes(s[end])){
-        subStr += s[end];
-        max = Math.max(subStr.length, max)
+      if(!hash[s[end]]) {
+        hash[s[end]] = 1;
+        end++;
+        max = Object.values(hash).length
       } else {
-        subStr = subStr.slice(start, end);
-        end++
+        delete hash[s[start]];
+        start++
       }
     }
-  return subStr
+      return max
 }
-// console.log(s.slice(2))
-console.log(lengthOfLongestSubstring(s))
