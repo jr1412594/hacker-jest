1558ddbc29206626d2fb61dc758f160ab4e6fbf1
jr1412594 <jr.medina1412@gmail.com>
2021-04-15 23:29:14 -0600

    start of function

---
+++ findMedian.js
@@ -0,0 +1,20 @@
+var findMedianSortedArrays = function(nums1, nums2) {
+    let joinedArray =  nums1.concat(nums2).sort((a,b) => a - b);
+    let left = 0;
+    let right = 0;
+    let count = joinedArray.length;
+    let result = 0
+  
+    if(count % 2 !== 0){
+      for(let i in joinedArray){
+        right = count - left - 1
+        if(right === left){
+          return joinedArray[i];
+          left++;
+        } else {
+          result = joinedArray[i+1] 
+        }
+      }
+    }
+      return result
+  }
